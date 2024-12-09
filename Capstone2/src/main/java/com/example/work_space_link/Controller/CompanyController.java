42bfbdfee33d732416e5b51b5eaefa075e76a20a
Ala'a <alaa.salenazi@gmail.com>
2024-12-09 10:10:18 +0300

    Add files via upload

---
+++ Capstone2/src/main/java/com/example/work_space_link/Controller/CompanyController.java
@@ -0,0 +1,67 @@
+package com.example.work_space_link.Controller;
+
+
+import com.example.work_space_link.ApiResponse.ApiResponse;
+import com.example.work_space_link.Model.Company;
+import com.example.work_space_link.Service.CompanyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/work-spase-link/company")
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @GetMapping("/get")
+    public ResponseEntity<?> getAllCompany() {
+        List<Company> companyList = companyService.getAllCompany();
+        return ResponseEntity.status(200).body(companyList);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addCompany(@RequestBody @Valid Company company, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(400).body(errors.getFieldError().getDefaultMessage());
+        }
+        companyService.addCompany(company);
+        return ResponseEntity.status(200).body(new ApiResponse("Company added successfully"));
+    }
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCompany(@PathVariable Integer id,@RequestBody @Valid Company company,Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(400).body(errors.getFieldError().getDefaultMessage());
+        }
+        companyService.updateCompany(id, company);
+        return ResponseEntity.status(200).body(new ApiResponse("Company updated successfully"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCompany(@PathVariable Integer id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Company deleted successfully"));
+    }
+
+    //endpoint
+    @GetMapping("/revenue-company/{id}")
+    public ResponseEntity<?> calculateRevenueOfCompany(@PathVariable Integer id){
+        Double revenue = companyService.calculateRevenueOfCompany(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Calculate the company's revenue = "+revenue));
+    }
+
+    //endpoint
+    @GetMapping("/revenue-company/id/{id}/from-date/{from}/to-date/{to}")
+    public ResponseEntity<?> calculateRevenueOfWorkspaceMonthly(@PathVariable Integer id, @PathVariable LocalDateTime from, @PathVariable LocalDateTime to){
+        Double revenue = companyService.calculateRevenueOfCompanyMonthly(id,from,to);
+        return ResponseEntity.status(200).body(new ApiResponse("Calculate the company's revenue for a specific period  = "+revenue));
+    }
+}
---
+++ Capstone2/src/main/java/com/example/work_space_link/Model/TimeSlot.java
@@ -0,0 +1,36 @@
+package com.example.work_space_link.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class TimeSlot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull(message = "Id of WorkSpace is required")
+    @Positive(message = "Id of WorkSpace must be Integer")
+    @Column(columnDefinition = "int not null")
+    private Integer workspaceId;
+
+    @NotNull(message = "Start date and time are required")
+    @Column(nullable = false)
+    private LocalDateTime startDateTime;
+
+    @NotNull(message = "End date and time are required")
+    @Column(nullable = false)
+    private LocalDateTime endDateTime;
+
+    @Column(nullable = false)
+    private Boolean isBooked;
+}
